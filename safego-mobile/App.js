82e5e2c0e4900a993ec78d7743cd6799e97635b7
aresoasis02 <gsyadav6195@gmail.com>
2026-08-06 19:28:56 +0530

    v1 with animations and modifications

--- safego-mobile/App.js
+++ safego-mobile/App.js
@@ -14,7 +14,7 @@ function LoadingScreen() {
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <ActivityIndicator size="large" />
     </View>
-  );
+  ); 
 }
 
 export default function App() {
